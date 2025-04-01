bb685f05c907ea3949afb3d2dc831587e4ff213a
Seyon <seyontennis@gmail.com>
2025-04-01 22:50:10 +0200

    Latest

--- oppgave6.py
+++ oppgave6.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-h = 0.01
+h = 0.25
 k = 0.01
 x = np.arange(0, 1+h, h)
 t = np.arange(0, 1+k, k)
@@ -14,8 +14,8 @@ r = k / h**2
 A = np.diag(2 * np.ones(n)) + np.diag(-1 * np.ones(n - 1), 1) + np.diag(-1 * np.ones(n - 1), -1)
 I = np.eye(n)
 
-M1 = I + r * A / 2
-M2 = I - r * A / 2
+M1 = 2*I + r * A
+M2 = 2*I - r * A
 
 for j in range(m - 1):
     u[1:-1, j+1] = np.linalg.solve(M1[1:-1, 1:-1], M2[1:-1, 1:-1] @ u[1:-1, j])
