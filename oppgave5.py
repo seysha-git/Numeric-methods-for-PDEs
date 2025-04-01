eb21d9dcb7a3457228fe71e396f729824c14cbbb
Seyon <seyontennis@gmail.com>
2025-04-01 21:46:48 +0200

    Latest

--- oppgave5.py
+++ oppgave5.py
@@ -4,8 +4,8 @@ import matplotlib.animation as animation
 
 h = 0.25
 k = 0.01
-x = np.arange(0, 1+h, h)
-t = np.arange(0, 1+k, k)
+x = np.arange(0, 1 + h, h)
+t = np.arange(0, 1 + k, k)
 n, m = len(x), len(t)
 u = np.zeros((n, m))
 u[:, 0] = np.sin(x)
@@ -13,12 +13,18 @@ r = k / h**2
 
 A = np.diag(2 * np.ones(n)) + np.diag(-1 * np.ones(n - 1), 1) + np.diag(-1 * np.ones(n - 1), -1)
 I = np.eye(n)
-M = np.linalg.inv(I + r * A)
+
+M = I + r * A
 
 for j in range(m - 1):
-    u[:, j+1] = M @ u[:, j]
+    u[1:-1, j + 1] = np.linalg.solve(M[1:-1, 1:-1], u[1:-1, j])
 
 fig, ax = plt.subplots()
 line, = ax.plot(x, u[:, 0])
-ani = animation.FuncAnimation(fig, lambda f: line.set_ydata(u[:, f]), frames=m, interval=100)
+
+def update(frame):
+    line.set_ydata(u[:, frame])
+    return line,
+
+ani = animation.FuncAnimation(fig, update, frames=m, interval=100)
 plt.show()
